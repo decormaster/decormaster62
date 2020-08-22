4d59a70ffcb8f2ac049aa5c51578402904a2bb1b
Dmitry Naumov <dmtr.number@gmail.com>
2020-08-22 20:21:58 +0300

    almost adaptive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
   $('.client-slider').slick({
     dots: true,
-    slidesToShow: 2
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+        }
+      }
+    ]
   });
 });
